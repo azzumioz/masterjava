6ed1ea398b10a585f829b733a6012d592e40d77a
azzumioz <azzumioz@gmail.com>
2020-10-01 21:35:00 +0700

    add JMH Benchmark

---
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixBenchmark.java
@@ -0,0 +1,60 @@
+package ru.javaops.masterjava.matrix;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@BenchmarkMode({Mode.SingleShotTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Threads(1)
+@Fork(10)
+@Timeout(time = 5, timeUnit = TimeUnit.MINUTES)
+public class MatrixBenchmark {
+
+    // Matrix size
+    private static final int MATRIX_SIZE = 1000;
+
+    @Param({"3", "4", "10"})
+    private int threadNumber;
+
+    private static int[][] matrixA;
+    private static int[][] matrixB;
+
+    @Setup
+    public void setUp() {
+        matrixA = MatrixUtil.create(MATRIX_SIZE);
+        matrixB = MatrixUtil.create(MATRIX_SIZE);
+    }
+
+    private ExecutorService executor;
+
+    //@Benchmark
+    public int[][] singleThreadMultiply() throws Exception {
+        return MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+    }
+
+    @Benchmark
+    public int[][] singleThreadMultiplyOpt() throws Exception {
+        return MatrixUtil.singleThreadMultiplyOptimize(matrixA, matrixB);
+    }
+
+    @Benchmark
+    public int[][] concurrentMultiply() throws Exception {
+        return MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+    }
+
+    @Setup
+    public void setup() {
+        executor = Executors.newFixedThreadPool(threadNumber);
+    }
+
+    @TearDown
+    public void tearDown() {
+        executor.shutdown();
+    }
+}
