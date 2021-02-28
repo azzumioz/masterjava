c531f5eea51852ae31cea0b461779eb3b735b695
azzumioz <azzumioz@gmail.com>
2021-02-28 11:55:25 +0700

    HW6 send email +send email with Apache commons email +use mail config mail.conf

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,40 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
+
 
 import java.util.List;
 
 @Slf4j
 public class MailSender {
+
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
+        Config mailConf = Configs.getConfig("mail.conf", "mail");
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName(mailConf.getString("host"));
+            email.setSmtpPort(mailConf.getInt("port"));
+            email.setAuthenticator(new DefaultAuthenticator(mailConf.getString("username"), mailConf.getString("password")));
+            email.setSSLOnConnect(mailConf.getBoolean("useSSL"));
+            email.setFrom(mailConf.getString("fromName"));
+            email.setSubject(subject);
+            email.setMsg(body);
+            for (Addressee addressee : to)
+            {
+                email.addTo(addressee.getEmail());
+            }
+            email.setStartTLSRequired(mailConf.getBoolean("useTLS"));
+            email.setDebug(mailConf.getBoolean("debug"));
+            email.send();
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
         log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
     }
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -1,6 +1,8 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
+import com.typesafe.config.Config;
+import ru.javaops.masterjava.config.Configs;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -14,7 +16,8 @@ public class MailServiceClient {
                 new URL("http://localhost:8080/mail/mailService?wsdl"),
                 new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService"));
 
+        Config mailConf = Configs.getConfig("mail.conf", "mail");
         MailService mailService = service.getPort(MailService.class);
-        mailService.sendMail(ImmutableList.of(new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+        mailService.sendMail(ImmutableList.of(new Addressee(mailConf.getString("to1"), null), new Addressee(mailConf.getString("to2"), null)), null, "Subject", "Body");
     }
 }
